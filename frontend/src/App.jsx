e93e758b4625b955b36fb5f42fc5e312ffa8b2d2
Prof-TiagoTrojahn <tiagotrojahn@gmail.com>
2025-08-20 18:33:12 -0300

    Adicionado codigo da atividade

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Seja bem vindo!</h1>
+      <p>Desenvolva seu website usando React!</p>
+    </>
+  )
+}
+
+export default App
