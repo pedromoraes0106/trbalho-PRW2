4e24982bfed50f9fccd8218907e2cf96c1513de9
Prof-TiagoTrojahn <tiagotrojahn@gmail.com>
2025-08-26 18:57:48 -0300

    Fix backend. Erro na digitação dos cabeçalhos permitidos

--- backend/compras.php
+++ backend/compras.php
@@ -8,7 +8,7 @@ require_once(__DIR__ . "/model/Usuarios.php");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Acess-Control-Alow-Headers: Content-Type, Authorzation");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if (method("OPTIONS")) {
     output(200, ["msg" => "OK"]);
--- backend/usuarios.php
+++ backend/usuarios.php
@@ -6,9 +6,9 @@ require_once(__DIR__ . "/model/Usuarios.php");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Acess-Control-Alow-Headers: Content-Type, Authorzation");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-if(method("OPTIONS")) {
+if (method("OPTIONS")) {
     output(200, ["msg" => "OK"]);
 }
 
